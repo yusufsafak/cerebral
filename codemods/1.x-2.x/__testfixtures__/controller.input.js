9850bd58575e95e18bb0140bf9305459d7db617f
Adam Hoddinott <adam.hoddinott@salespreso.com>
2017-01-24 08:38:28 +1100

    feat(codemods): add complete controller mod

--- codemods/1.x-2.x/__testfixtures__/controller.input.js
+++ codemods/1.x-2.x/__testfixtures__/controller.input.js
@@ -2,9 +2,22 @@
 import {Controller} from 'cerebral'
 import Model from 'cerebral/models/immutable'
 
+import Home from './modules/Home'
+import Admin from './modules/Admin'
+import doSomething from './chains/doSomething'
+
 const controller = Controller(Model({
   // You can add some initial state here if you want
   foo: 'bar'
 }))
+
+controller.addModules({
+  home: Home,
+  admin: Admin
+})
+
+controller.addSignals({
+  buttonClicked: doSomething
+})
 
 export default controller
--- codemods/1.x-2.x/controller.js
+++ codemods/1.x-2.x/controller.js
@@ -27,11 +27,57 @@ module.exports = function (fileInfo, api) {
     return null
   }
 
-  const newObject = j.objectExpression([
-  	j.property('init', j.identifier("state"), objectExpression.get(0).node)
-  ])
+  const expressions = []
 
-  modelInstances.replaceWith(newObject)
+  expressions.push(
+    j.property('init', j.identifier('state'), objectExpression.get(0).node)
+  )
+
+  const controllerVariableName = modelInstances.get(0).parent.parent.value.id.name
+
+  // Find any expressions that use the controller variable
+  const controllerExpressions = root.find(j.ExpressionStatement, {
+    expression: {
+      callee: {
+        object: {
+          name: controllerVariableName
+        }
+      }
+    }
+  })
+
+  controllerExpressions.forEach((exp) => {
+    // Fix the name
+    const name = exp.value.expression.callee.property.name
+      .replace('add', '').toLowerCase()
+
+    let expression = exp.value.expression.arguments[0]
+
+    // Modules are now called, so let's conver them
+    // to a call expression
+    if (name === 'modules') {
+      expression.properties = expression.properties.map((propExp) => {
+        propExp.value = j.callExpression(
+          j.identifier(propExp.value.name),
+          []
+        )
+        return propExp
+      })
+    }
+
+    expressions.push(
+      j.property(
+        'init',
+        j.identifier(name),
+        expression
+      )
+    )
+
+    // Remove the existing separated version
+    exp.prune()
+  })
+
+  modelInstances.replaceWith(j.objectExpression(expressions))
 
   return root.toSource()
 }
