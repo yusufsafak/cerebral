75f232318a75ebf9f24d981af6789400c319f669
yusufsafak <yusufsafak@yahoo.com>
2017-03-23 17:01:28 +0100

    test(form): add test update field error message

--- packages/cerebral-provider-forms/src/index.test.js
+++ packages/cerebral-provider-forms/src/index.test.js
@@ -1,6 +1,8 @@
 /* eslint-env mocha */
 import {Controller} from 'cerebral'
+import {state} from 'cerebral/tags'
 import FormsProvider from '.'
+import setField from './operators/setField'
 import assert from 'assert'
 import rules from './rules'
 
@@ -135,6 +137,56 @@ describe('provider', () => {
       }
     })
     controller.getSignal('test')()
+    rules._errorMessages = {}
+  })
+  it('should be able to update field error message', () => {
+    const controller = Controller({
+      providers: [
+        FormsProvider({
+          errorMessages: {
+            minLength (value, minLengthValue) {
+              return `is length ${value.length}, should be equal or more than ${minLengthValue}`
+            },
+            isEmail (value) {
+              return `${value} is not valid email address`
+            }
+          }
+        })
+      ],
+      state: {
+        form: {
+          email: {
+            value: 'be',
+            validationRules: ['minLength:3', 'isEmail']
+          }
+        }
+      },
+      signals: {
+        test: [
+          ({forms}) => {
+            const form = forms.get('form')
+            assert.equal(form.isValid, false)
+            assert.equal(form.email.value, 'be')
+            assert.equal(form.email.isValid, false)
+            assert.equal(form.email.failedRule.name, 'minLength')
+            assert.equal(form.email.failedRule.arg, 3)
+            assert.equal(form.email.errorMessage, 'is length 2, should be equal or more than 3')
+          }
+        ],
+        changeEmail: [
+          setField(state`form.email`, 'ben@example.com'),
+          ({forms}) => {
+            const form = forms.get('form')
+            assert.equal(form.email.value, 'ben@example.com')
+            assert.equal(form.email.isValid, true)
+            assert.equal(form.email.failedRule, null)
+            assert.equal(form.email.errorMessage, undefined)
+          }
+        ]
+      }
+    })
+    controller.getSignal('test')()
+    controller.getSignal('changeEmail')()
     rules._errorMessages = {}
   })
   it('should be able to reset form', () => {
